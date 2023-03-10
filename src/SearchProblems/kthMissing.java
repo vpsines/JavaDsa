cfb37b6ea2bad016dcd7fa54748ffce106ac21d3
vpsines <sasip6028@gmail.com>
2023-03-10 12:08:35 +0530

    problem: kth missing using binary search

--- src/SearchProblems/kthMissing.java
+++ src/SearchProblems/kthMissing.java
@@ -4,7 +4,10 @@ public class kthMissing {
 
     public static void main(String[] args) {
         int[] arr ={2,3,4,7,11};
-        System.out.println(findKthMissing(arr,
+
+        // 2 3 4 7 11
+        // 0 1 2 3 4
+        System.out.println(findKthMissing2(arr,
                 5));
     }
 
@@ -22,4 +25,18 @@ public class kthMissing {
         return i-1;
     }
 
+    public static int findKthMissing2(int[] arr,int k){
+        int low = 0;
+        int high = arr.length-1;
+        int mid = 0;
+        while (low <= high){
+            mid = low + (high-low)/2;
+            if(arr[mid] - (mid +1) >= k){
+                high = mid-1;
+            }else {
+                low = mid+1;
+            }
+        }
+        return low+k;
+    }
 }
